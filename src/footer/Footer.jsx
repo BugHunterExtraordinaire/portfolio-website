329a9eec4cfea123fac057c0d5c1dc6115d7b585
Tarek Abdelhamid Mohamed Abdelhamid <tarek.mohamed.bughunter@gmail.com>
2026-02-13 23:46:23 +0200

    Finalize website layout and styles

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,7 +1,34 @@
 export default function Footer() {
   return (
-    <footer className="w-full">
-      
+    <footer className="w-full flex flex-col mt-15 border-t py-5" id="contact">
+      <h2 className="text-6xl text-center font-bold">Have a project in mind? Contact Me!</h2>
+      <nav aria-label="Social Contacts" className="pt-5">
+        <ul className="flex justify-evenly text-2xl">
+          <li>
+            <a href="tel:+201067297886" 
+               className="hover:text-(--theme-color) duration-100 ease-in">
+              <i className="fa-solid fa-phone" 
+                 aria-hidden="true" /> Phone me
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/tarek-mohamed-6261ab368/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BdsG90pybTPGx%2F3ksJ34uNQ%3D%3D" 
+               target="_blank" 
+               className="hover:text-(--theme-color) duration-100 ease-in">
+              <i className="fa-brands fa-linkedin" 
+                 aria-hidden='true' /> Message me on LinkedIn
+            </a>
+          </li>
+          <li>
+            <a href="mailto:tarek.mohamed.bughunter@gmail.com"
+               target="_blank"
+               className="hover:text-(--theme-color) duration-100 ease-in">
+                <i className="fa-solid fa-envelope" 
+                   aria-hidden="true" /> Email me
+            </a>
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 }
--- src/main-content/about-components/Socials.jsx
+++ src/main-content/about-components/Socials.jsx
@@ -5,9 +5,9 @@ export default function Socials() {
     <section aria-label='My Socials' className='pt-12 w-full flex gap-6'>
       <a href='https://www.linkedin.com/in/tarek-mohamed-6261ab368/'
          target='_blank'
-         className='text-[35px] border-2 w-12.5 rounded-lg text-center linkedin hover:duration-100 ease-in'
-         aria-label='My Linkedin'>
-          <i className="fa-brands fa-linkedin-in" aria-hidden='true' />
+         className='text-[35px] border-2 w-12.5 rounded-lg text-center x hover:duration-100 ease-in'
+         aria-label='My Twitter (X)'>
+          <i class="fa-brands fa-x-twitter" aria-hidded="true"></i>
       </a>
       <a href='https://www.facebook.com/tarek.abdelhamid.5437/'
          target='_blank'
@@ -20,6 +20,12 @@ export default function Socials() {
          className='text-[35px] border-2 w-12.5 rounded-lg text-center instagram hover:duration-100 ease-in'
          aria-label='My Instagram'>
           <i className="fa-brands fa-instagram" aria-hidden='true' />
+      </a>
+      <a href='https://github.com/BugHunterExtraordinaire'
+         target='_blank'
+         className='text-[35px] border-2 w-12.5 rounded-lg text-center x hover:duration-100 ease-in'
+         aria-label='My GitHub'>
+          <i class="fa-brands fa-github" aria-hidden='true' />
       </a>
     </section>
   );
